cf9c92d781124df35d6c9deca369cb85084c028f
Roman Surkov <surkovr@list.ru>
2020-09-16 08:27:59 +0900

    Fixes and backspace, x*x, 1divX actions added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,103 +9,78 @@ import java.math.RoundingMode;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static java.util.Objects.isNull;
+
 public class Controller {
-
-    // TODO SCALE !!!
-    public static final int SCALE = 7;
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
-
-    @FXML
-    private Button btnPercent;
-
-    @FXML
-    private Button btnClearAll;
-
-    @FXML
-    private Button btnXmultiplyX;
-
-    @FXML
-    private Button btnRoot;
-
-    @FXML
-    private Button btn1divByX;
-
-    @FXML
-    private Button btnClearCurrent;
-
-    @FXML
-    private Button btnBackspace;
-
-    @FXML
-    private Button btnDivide;
-
-    @FXML
-    private Button btn7;
-
-    @FXML
-    private Button btn8;
-
-    @FXML
-    private Button btn9;
-
-    @FXML
-    private Button btnX;
-
-    @FXML
-    private Button btn4;
-
-    @FXML
-    private Button btn5;
-
-    @FXML
-    private Button btn6;
-
-    @FXML
-    private Button btnMinus;
-
-    @FXML
-    private Button btn1;
-
-    @FXML
-    private Button btn2;
-
-    @FXML
-    private Button btn3;
-
-    @FXML
-    private Button btnPlus;
-
-    @FXML
-    private Button btn0;
-
-    @FXML
-    private Button btnComma;
-
-    @FXML
-    private Button btnEquals;
-
-    @FXML
-    private Label labelField;
-
-    @FXML
-    private Button btnMemClear;
-
-    @FXML
-    private Button btnMemReturn;
-
-    @FXML
-    private Button btnMemPlus;
-
-    @FXML
-    private Button btnMemMinus;
 
     private String stringNum = "";
     private BigDecimal firstNum;
+    private BigDecimal secondNum;
     private char operation;
+
+    // TODO исправить результат очень больших чесел на е в степени
+    // TODO запоминать промежуточный результат. (2 + 3 = 5, далее + 4 = должо быть 9)
+    public static final int SCALE = 7;
+    @FXML
+    private ResourceBundle resources;
+    @FXML
+    private URL location;
+    @FXML
+    private Button btnPercent;
+    @FXML
+    private Button btnClearAll;
+    @FXML
+    private Button btnXmultiplyX;
+    @FXML
+    private Button btnRoot;
+    @FXML
+    private Button btn1divByX;
+    @FXML
+    private Button btnClearCurrent;
+    @FXML
+    private Button btnBackspace;
+    @FXML
+    private Button btnDivide;
+    @FXML
+    private Button btn7;
+    @FXML
+    private Button btn8;
+    @FXML
+    private Button btn9;
+    @FXML
+    private Button btnX;
+    @FXML
+    private Button btn4;
+    @FXML
+    private Button btn5;
+    @FXML
+    private Button btn6;
+    @FXML
+    private Button btnMinus;
+    @FXML
+    private Button btn1;
+    @FXML
+    private Button btn2;
+    @FXML
+    private Button btn3;
+    @FXML
+    private Button btnPlus;
+    @FXML
+    private Button btn0;
+    @FXML
+    private Button btnComma;
+    @FXML
+    private Button btnEquals;
+    @FXML
+    private Label labelField;
+    @FXML
+    private Button btnMemClear;
+    @FXML
+    private Button btnMemReturn;
+    @FXML
+    private Button btnMemPlus;
+    @FXML
+    private Button btnMemMinus;
 
     @FXML
     void initialize() {
@@ -167,55 +142,67 @@ public class Controller {
                 labelField.setText(stringNum);
             }
         });
-        btnClearAll.setOnAction(actionEvent -> {
+        btnClearAll.setOnAction(event -> {
             labelField.setText("0");
             this.stringNum = "";
             this.firstNum = BigDecimal.ZERO;
             this.operation = 'E';
         });
-        // +- button = *-1
-//        assert btnPercent != null : "fx:id=\"btnPercent\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btnClearAll != null : "fx:id=\"btnClearAll\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btnXmultipleX != null : "fx:id=\"btnXmultipleX\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btnRoot != null : "fx:id=\"btnRoot\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btn1divByX != null : "fx:id=\"btn1divByX\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btnClearCurrent != null : "fx:id=\"btnClearCurrent\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btnBackspace != null : "fx:id=\"btnBackspace\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btnDivide != null : "fx:id=\"btnDivide\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btn7 != null : "fx:id=\"btn7\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btn8 != null : "fx:id=\"btn8\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btn9 != null : "fx:id=\"btn9\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btnX != null : "fx:id=\"btnX\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btn4 != null : "fx:id=\"btn4\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btn5 != null : "fx:id=\"btn5\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btn6 != null : "fx:id=\"btn6\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btnMinus != null : "fx:id=\"btnMinus\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btn1 != null : "fx:id=\"btn1\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btn2 != null : "fx:id=\"btn2\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btn3 != null : "fx:id=\"btn3\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btnPlus != null : "fx:id=\"btnPlus\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btn0 != null : "fx:id=\"btn0\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btnComma != null : "fx:id=\"btnComma\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btnEquals != null : "fx:id=\"btnEquals\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert labelField != null : "fx:id=\"labelField\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btnMemClear != null : "fx:id=\"btnMemClear\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btnMemReturn != null : "fx:id=\"btnMemReturn\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btnMemPlus != null : "fx:id=\"btnMemPlus\" was not injected: check your FXML file 'sample.fxml'.";
-//        assert btnMemMinus != null : "fx:id=\"btnMemMinus\" was not injected: check your FXML file 'sample.fxml'.";
+        btnXmultiplyX.setOnAction(event -> {
+            String text = labelField.getText();
+            squareAction(text);
+        });
+        btn1divByX.setOnAction(event -> {
+            String text = labelField.getText();
+            oneDivByXAction(text);
+        });
+        btnBackspace.setOnAction(event -> {
+            removeNumber();
+        });
+    }
 
+    private void oneDivByXAction(String text) {
+        String textToShow = null;
+        if (isNull(text) || "".equals(text) || text.equals("0")) {
+            textToShow = "0";
+        } else {
+            double parseDouble = Double.parseDouble(text);
+            BigDecimal x = BigDecimal.valueOf(parseDouble);
+            BigDecimal res = BigDecimal.ONE.divide(x, SCALE, RoundingMode.HALF_UP);
+            textToShow = getTextToShow(res);
+        }
+
+        labelField.setText(textToShow);
+        this.operation = 'E';
+        this.firstNum = BigDecimal.ZERO;
+        this.stringNum = "";
+    }
+
+    private void squareAction(String text) {
+        double parseDouble = Double.parseDouble(text);
+        BigDecimal x = BigDecimal.valueOf(parseDouble);
+        BigDecimal res = x.multiply(x);
+        String textToShow = getTextToShow(res);
+
+        labelField.setText(textToShow);
+        this.operation = 'E';
+        this.firstNum = BigDecimal.ZERO;
+        this.stringNum = "";
     }
 
     void equalAction() {
 
         if (this.operation == '+' || this.operation == '-' || this.operation == '/' || this.operation == '*') {
             BigDecimal res = BigDecimal.ZERO;
-            final BigDecimal value = BigDecimal.valueOf(Double.parseDouble(this.stringNum));
+
+            secondNum = BigDecimal.valueOf(Double.parseDouble(this.stringNum));
+
             switch (this.operation) {
-                case '+' -> res = this.firstNum.add(value);
-                case '-' -> res = this.firstNum.subtract(value);
-                case '*' -> res = this.firstNum.multiply(value);
+                case '+' -> res = this.firstNum.add(secondNum);
+                case '-' -> res = this.firstNum.subtract(secondNum);
+                case '*' -> res = this.firstNum.multiply(secondNum);
                 case '/' -> {
-                    BigDecimal divisor = value;
+                    BigDecimal divisor = secondNum;
                     if (divisor.signum() != 0) {
                         res = this.firstNum.divide(divisor, SCALE, RoundingMode.HALF_UP);
                     } else {
@@ -224,40 +211,60 @@ public class Controller {
                 }
             }
 
-            String textToShow = res.toPlainString();
-            if (textToShow.matches("[0-9]+[.][0-9]+[0]+")) {
-                char[] chars = textToShow.toCharArray();
-                int lastIndex = chars.length - 1;
-                for (int i = chars.length - 1; i >= 0; i--) {
-                    if (chars[i] != '0') break;
-                    lastIndex--;
-                }
-
-                StringBuilder tempRes = new StringBuilder();
-                for (int i = 0; i <= lastIndex; i++) {
-                    tempRes.append(chars[i]);
-                }
-                textToShow = tempRes.toString();
-            }
-
+            String textToShow = getTextToShow(res);
+            this.firstNum = res;
             labelField.setText(textToShow);
-            this.operation = 'E';
-            this.firstNum = BigDecimal.ZERO;
-            this.stringNum = "";
+//            this.operation = 'E';
+//            this.firstNum = BigDecimal.ZERO;
+//            this.stringNum = "";
         }
     }
 
+    private String getTextToShow(BigDecimal res) {
+        if (isNull(res)) {
+            return "";
+        }
+        String textToShow = res.toPlainString();
+        if (textToShow.matches("[0-9]+[.][0-9]+[0]+")) {
+            char[] chars = textToShow.toCharArray();
+            int lastIndex = chars.length - 1;
+            for (int i = chars.length - 1; i >= 0; i--) {
+                if (chars[i] != '0') {
+                    break;
+                }
+                lastIndex--;
+            }
+
+            StringBuilder tempRes = new StringBuilder();
+            for (int i = 0; i <= lastIndex; i++) {
+                tempRes.append(chars[i]);
+            }
+            textToShow = tempRes.toString();
+        }
+        if (textToShow.endsWith(".")) {
+            textToShow = textToShow.replaceAll("\\.", "");
+        }
+        return textToShow;
+    }
+
     void mathAction(char operation) {
-        if (this.operation != '+' && this.operation != '-' && this.operation != '/' && this.operation != '*') {
-            this.firstNum = BigDecimal.valueOf(Double.parseDouble(this.stringNum));
+//        if (this.operation != '+' && this.operation != '-' && this.operation != '/' && this.operation != '*') {
+            secondNum = BigDecimal.ZERO;
+            firstNum = BigDecimal.valueOf(Double.parseDouble(this.stringNum));
             labelField.setText(String.valueOf(operation));
             this.stringNum = "";
             this.operation = operation;
-        }
+//        }
     }
 
     void addNumber(int number) {
         this.stringNum += Integer.toString(number);
+        labelField.setText(this.stringNum);
+    }
+
+    void removeNumber() {
+        String text = labelField.getText();
+        this.stringNum = text.substring(0, text.length() - 1);
         labelField.setText(this.stringNum);
     }
 }
